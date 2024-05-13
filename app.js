329021dcf28c887fa26b20156917431a7d9d8909
aamir <aamirchanna1172gmail.com>
2024-05-13 12:47:01 -0700

    add css

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 let counterr = 0;
 let count1 =   document.getElementById("count");
 let reset2 = document.getElementById("reset");
+let even2 = document.getElementById("even")
+let odd2 = document.getElementById("odd");
 function counter(){
  counterr+=1;
   count1.innerHTML = counterr;
+  
 }
-
-
 
 function dec(){
 
-    let decrease1 = document.getElementById("decrease");
+ let decrease1 = document.getElementById("decrease");
 counterr-=1
 count1.innerHTML = counterr;
 }
